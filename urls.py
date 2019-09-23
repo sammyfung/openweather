a8779a8dc6c699656a7030e89587c57b9e9a4ff4
Sammy Fung <sammy@sammy.hk>
2019-09-23 22:00:18 +0800

    deprecated patterns() API

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^airquality/hourly/$', views.hourly_data),
     url(r'^airqualitystation/$', views.station_data),
     url(r'^regional/$', views.regional_weather_data),
-)
+]
